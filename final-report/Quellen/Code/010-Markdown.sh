9b7a7f7e4bd9727518e3feb7920d73f801d98904
Adrian Wilke <adibaba@users.noreply.github.com>
2021-03-17 23:44:33 +0100

    report debug

--- final-report/Quellen/Code/010-Markdown.sh
+++ final-report/Quellen/Code/010-Markdown.sh
@@ -2,5 +2,15 @@
 
 mkdir -p ../../Tmp
 #cat ../*.md > ../../Tmp/content.md
-#cat ../2*.md ../3*.md ../a*.md > ../../Tmp/content.md
-cat ../8*.md ../9*.md ../a*.md ../b*.md > ../../Tmp/content.md
+
+# Errors, probably tables
+#cat ../1*.md  > ../../Tmp/content.md
+#cat ../2*.md  > ../../Tmp/content.md
+#cat ../3*.md  > ../../Tmp/content.md
+
+cat ../4*.md ../5*.md ../6*.md ../7*.md ../8*.md ../9*.md ../a*.md ../b*.md > ../../Tmp/content.md
+
+#cat ../1*.md ../2*.md ../3*.md ../a*.md > ../../Tmp/content.md
+#cat ../4*.md ../5*.md ../6*.md ../a*.md > ../../Tmp/content.md
+#cat ../7*.md ../8*.md ../9*.md ../a*.md > ../../Tmp/content.md
+#cat ../8*.md ../9*.md ../a*.md ../b*.md > ../../Tmp/content.md
--- final-report/Quellen/Code/030-Pandoc.sh
+++ final-report/Quellen/Code/030-Pandoc.sh
@@ -11,11 +11,19 @@ pandoc content.md \
 --number-sections \
 --listings \
 --toc \
+--pdf-engine=pdflatex \
 -V page-background=background.pdf \
 -V titlepage=true \
 -V titlepage-background=title.pdf \
 -V titlepage-rule-height=0 \
 -V code-block-font-size="\footnotesize" \
 -V lang=de
+
+# Pandoc <  v2: markdown_github+raw_tex
+# Pandoc 2.12: "[WARNING] Deprecated: markdown_github. Use gfm instead."
+# Pandoc 2.12: Table errors
+# Pandoc 2.12: gfm -> no newline, no table width?
+
+# --verbose \
 
 popd > /dev/null
